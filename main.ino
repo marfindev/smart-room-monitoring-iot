7c6f60e8bc89b05b199b000f8685527be3869f0a
Marfin Pro <arsyafin@gmail.com>
2026-03-04 15:33:22 +0700

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,49 @@
+#define BLYNK_AUTH_TOKEN "XAxKGiz3o8bZAant3E8oAQpNpnjVKHwd"
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+ 
+
+#include <DHT.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "Teras Malikah";  // type your wifi name
+char pass[] = "malikahku19";  // type your wifi password
+
+#define DHTPIN D6          // Mention the digital pin where you connected 
+#define DHTTYPE DHT11     // DHT 11  
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+
+
+void setup(){
+   Serial.begin(115200);
+  
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  dht.begin();
+  timer.setInterval(2500L, sendSensor);
+}
+
+void loop(){
+  Blynk.run();
+  timer.run();
+}
+void sendSensor(){
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Blynk.virtualWrite(V4, t);
+  Blynk.virtualWrite(V5, h);
+  
+  Serial.print("Suhu : ");
+  Serial.print(t);
+  Serial.print("|| Kelembapan : ");
+  Serial.println(h);
+  
+}
